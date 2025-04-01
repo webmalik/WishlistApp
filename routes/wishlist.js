18e70e3c51d79d227146317c87d402fe47aa8131
Dmytro Adamovych <dmytro.adamovych@gmail.com>
2025-04-02 02:18:40 +0300

    Update GET

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -103,4 +103,52 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/get', async (req, res) => {
+    const { customerId, shop } = req.query;
+
+    const allowedShops = ['dev-paka.myshopify.com'];
+
+    if (!allowedShops.includes(shop)) {
+        return res.status(403).json({ error: 'Invalid shop' });
+    }
+
+    if (!customerId) {
+        return res.status(400).json({ error: 'Missing customerId' });
+    }
+
+    const token = SHOPIFY_API_TOKEN;
+    if (!token) {
+        return res.status(401).json({ error: 'No token' });
+    }
+
+    try {
+        const { data } = await axios.get(
+            `https://${shop}/admin/api/${API_VERSION}/customers/${customerId}/metafields.json`,
+            {
+                headers: {
+                    'X-Shopify-Access-Token': token,
+                    'Content-Type': 'application/json',
+                },
+            },
+        );
+
+        const metafield = data.metafields.find(
+            (m) => m.namespace === 'custom' && m.key === 'wishlist',
+        );
+
+        let wishlist = [];
+
+        if (metafield?.value) {
+            wishlist = JSON.parse(metafield.value.replace(/gid:\/\/shopify\/Product\//g, '')).map(
+                Number,
+            );
+        }
+
+        return res.json({ success: true, wishlist });
+    } catch (err) {
+        console.error('[Wishlist GET error]', err?.response?.data || err.message);
+        return res.status(500).json({ error: 'Internal error loading wishlist' });
+    }
+});
+
 module.exports = router;
